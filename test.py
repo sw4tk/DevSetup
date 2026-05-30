09c8a3218e116e718fbe69cea409e6f6410bdff9
sw4tk <143151894+sw4tk@users.noreply.github.com>
2026-05-30 23:40:51 +0530

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-di = {
-    "python": {"installed": True, "version": "Python 3.14.3"}, "git": {"installed": False, "version": None}, "pip": {"installed": True, "version": "pip 26.1 from C:\\Users\\DELL\\AppData\\Roaming\\Python\\Python314\\site-packages\\pip (python 3.14)"}
-}
-
-for tool,items in di.items():
-    print(tool,items)
